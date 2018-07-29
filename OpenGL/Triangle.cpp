8a77f07f280e7cc8971794854b07e961acbb3a82
Leonardo H. Añez Vladimirovna <nozosimo@gmail.com>
2018-07-29 18:41:45 -0400

    Create Triangle.cpp

---
+++ OpenGL/Triangle.cpp
@@ -0,0 +1,254 @@
+#include <iostream>
+#include <sstream>
+#define GLEW_STATIC
+#include "GL/glew.h"
+#include "GLFW/glfw3.h"
+
+
+// Global Variables
+const char* APP_TITLE = "Triangle Extension to Square";
+const int gWindowWidth = 800;
+const int gWindowHeight = 600;
+GLFWwindow* gWindow = NULL;
+bool gWireframe = false;
+
+// Shaders, ni la mas perra Idea de esta parte
+const GLchar* vertexShaderSrc = 
+"#version 330 core\n"
+"layout (location = 0) in vec3 pos;"
+"void main()"
+"{"
+"   gl_Position = vec4(pos.x, pos.y, pos.z, 1.0);"
+"}";
+const GLchar* fragmentShaderSrc = 
+"#version 330 core\n"
+"out vec4 frag_color;"
+"void main()"
+"{"
+"   frag_color = vec4(0.35f, 0.96f, 0.3f, 1.0f);"
+"}";
+
+
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode);
+void glfw_onFramebufferSize(GLFWwindow* window, int width, int height);
+void showFPS(GLFWwindow* window);
+bool initOpenGL();
+
+
+int main()
+{
+	if (!initOpenGL())
+	{
+
+		std::cerr << "GLFW initialization failed" << std::endl;
+		return -1;
+	}
+
+	GLfloat vertices[] = {
+		-0.5f,  0.5f, 0.0f,		// Top left
+		0.5f,  0.5f, 0.0f,		// Top right
+		0.5f, -0.5f, 0.0f,		// Bottom right
+		-0.5f, -0.5f, 0.0f		// Bottom left 
+	};
+
+	GLuint indices[] = {
+		0, 1, 2,  // First Triangle
+		0, 2, 3   // Second Triangle
+	};
+
+	GLuint vbo, ibo, vao;
+
+	glGenBuffers(1, &vbo);					
+	glBindBuffer(GL_ARRAY_BUFFER, vbo);		
+	glBufferData(GL_ARRAY_BUFFER, sizeof(vertices), vertices, GL_STATIC_DRAW);	
+
+	glGenVertexArrays(1, &vao);				
+	glBindVertexArray(vao);					
+	glVertexAttribPointer(0, 3, GL_FLOAT, GL_FALSE, 0, NULL);	
+	glEnableVertexAttribArray(0);			
+
+										
+	glGenBuffers(1, &ibo);	
+	glBindBuffer(GL_ELEMENT_ARRAY_BUFFER, ibo);
+	glBufferData(GL_ELEMENT_ARRAY_BUFFER, sizeof(indices), indices, GL_STATIC_DRAW);
+
+
+
+	GLuint vs = glCreateShader(GL_VERTEX_SHADER);
+	glShaderSource(vs, 1, &vertexShaderSrc, NULL);
+	glCompileShader(vs);
+
+	// Check for compile errors
+	GLint result;
+	GLchar infoLog[512];
+	glGetShaderiv(vs, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(vs, sizeof(infoLog), NULL, infoLog);
+		std::cout << "Error! Vertex shader failed to compile. " << infoLog << std::endl;
+	}
+
+
+	GLint fs = glCreateShader(GL_FRAGMENT_SHADER);
+	glShaderSource(fs, 1, &fragmentShaderSrc, NULL);
+	glCompileShader(fs);
+
+	// Check for compile errors
+	glGetShaderiv(fs, GL_COMPILE_STATUS, &result);
+	if (!result)
+	{
+		glGetShaderInfoLog(fs, sizeof(infoLog), NULL, infoLog);
+		std::cout << "Error! Fragment shader failed to compile. " << infoLog << std::endl;
+	}
+
+	GLint shaderProgram = glCreateProgram();
+	glAttachShader(shaderProgram, vs);
+	glAttachShader(shaderProgram, fs);
+	glLinkProgram(shaderProgram);
+
+	// Check for linker errors
+	glGetProgramiv(shaderProgram, GL_LINK_STATUS, &result);
+	if (!result)
+	{
+		glGetProgramInfoLog(shaderProgram, sizeof(infoLog), NULL, infoLog);
+		std::cout << "Error! Shader program linker failure. " << infoLog  << std::endl;
+	}
+
+	// Clean up shaders
+	glDeleteShader(vs);
+	glDeleteShader(fs);
+
+
+	// Rendering loop
+	while (!glfwWindowShouldClose(gWindow))
+	{
+		showFPS(gWindow);
+
+		// Poll for and process events
+		glfwPollEvents();
+
+		// Clear the screen
+		glClear(GL_COLOR_BUFFER_BIT);
+
+		// Render the quad (two triangles)
+		glUseProgram(shaderProgram);
+		glBindVertexArray(vao);
+		glDrawElements(GL_TRIANGLES, 6, GL_UNSIGNED_INT, 0);
+		glBindVertexArray(0);
+
+		// Swap front and back buffers
+		glfwSwapBuffers(gWindow);
+	}
+
+	// Clean up
+	glDeleteProgram(shaderProgram);
+	glDeleteVertexArrays(1, &vao);
+	glDeleteBuffers(1, &vbo);
+	glDeleteBuffers(1, &ibo);
+
+	glfwTerminate();
+
+	return 0;
+}
+
+
+bool initOpenGL()
+{
+	// Intialize GLFW 
+	if (!glfwInit())
+	{
+		// An error
+		std::cerr << "GLFW initialization failed" << std::endl;
+		return false;
+	}
+
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MAJOR, 3);
+	glfwWindowHint(GLFW_CONTEXT_VERSION_MINOR, 3);
+	glfwWindowHint(GLFW_OPENGL_PROFILE, GLFW_OPENGL_CORE_PROFILE);
+	glfwWindowHint(GLFW_OPENGL_FORWARD_COMPAT, GL_TRUE);	
+
+															
+	gWindow = glfwCreateWindow(gWindowWidth, gWindowHeight, APP_TITLE, NULL, NULL);
+	if (gWindow == NULL)
+	{
+		std::cerr << "Failed to create GLFW window" << std::endl;
+		glfwTerminate();
+		return false;
+	}
+
+	// Make the window's context the current one
+	glfwMakeContextCurrent(gWindow);
+
+	// Initialize GLEW
+	glewExperimental = GL_TRUE;
+	if (glewInit() != GLEW_OK)
+	{
+		std::cerr << "Failed to initialize GLEW" << std::endl;
+		return false;
+	}
+
+	// Set the required callback functions
+	glfwSetKeyCallback(gWindow, glfw_onKey);
+	glfwSetFramebufferSizeCallback(gWindow, glfw_onFramebufferSize);
+
+	glClearColor(0.23f, 0.38f, 0.47f, 1.0f);
+
+	// Define the viewport dimensions
+	glViewport(0, 0, gWindowWidth, gWindowHeight);
+
+	return true;
+}
+
+void glfw_onKey(GLFWwindow* window, int key, int scancode, int action, int mode)
+{
+	if (key == GLFW_KEY_ESCAPE && action == GLFW_PRESS)
+		glfwSetWindowShouldClose(window, GL_TRUE);
+
+	if (key == GLFW_KEY_W && action == GLFW_PRESS)
+	{
+		gWireframe = !gWireframe;
+		if (gWireframe)
+			glPolygonMode(GL_FRONT_AND_BACK, GL_LINE);
+		else
+			glPolygonMode(GL_FRONT_AND_BACK, GL_FILL);
+	}
+}
+
+
+void glfw_onFramebufferSize(GLFWwindow* window, int width, int height)
+{
+	glViewport(0, 0, width, height);
+}
+
+
+void showFPS(GLFWwindow* window)
+{
+	static double previousSeconds = 0.0;
+	static int frameCount = 0;
+	double elapsedSeconds;
+	double currentSeconds = glfwGetTime(); 
+
+	elapsedSeconds = currentSeconds - previousSeconds;
+
+	// Limit text updates to 4 times per second
+	if (elapsedSeconds > 0.25)
+	{
+		previousSeconds = currentSeconds;
+		double fps = (double)frameCount / elapsedSeconds;
+		double msPerFrame = 1000.0 / fps;
+
+		// The C++ way of setting the window title
+		std::ostringstream outs;
+		outs.precision(3);	// decimal places
+		outs << std::fixed
+			<< APP_TITLE << "    "
+			<< "FPS: " << fps << "    "
+			<< "Frame Time: " << msPerFrame << " (ms)";
+		glfwSetWindowTitle(window, outs.str().c_str());
+
+		// Reset for next average.
+		frameCount = 0;
+	}
+
+	frameCount++;
+}
